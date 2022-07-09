e3a1373bc857027e1eb194689058701bfc0535e5
thorgull <thorgull@gmail.com>
2022-07-09 11:27:16 +0200

    add all in one setup file

--- setup.sh
+++ setup.sh
@@ -1,11 +1,4 @@
-# oh-my-posh-themes
-
-## linux install system wide
-```bash
+#!/usr/bin/env sh
 sudo curl "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64" -L -o /usr/local/bin/oh-my-posh
 sudo curl "https://raw.githubusercontent.com/thorgull/oh-my-posh-themes/main/etc/profile.d/oh-my-posh-init.sh" -L -o /etc/profile.d/oh-my-posh-init.sh
-```
-or
-```
-curl -L https://raw.githubusercontent.com/thorgull/oh-my-posh-themes/main/setup.sh | sh
-```
+. /etc/profile.d/oh-my-posh-init.sh
